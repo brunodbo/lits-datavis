e60b8798bf3dc549b7c552872158ccdfc81d2364
Bruno De Bondt <mail@brunodbo.be>
2014-02-02 21:33:50 -0800

    Add spill risk data

--- rollans_report/js/pipeline_sections.js
+++ rollans_report/js/pipeline_sections.js
@@ -7,50 +7,23 @@ $(function() {
   var routeColor = '#e44735';
   var routeStyle = {
     'className': 'pipeline',
-    // 'color': routeColor,
-    // 'fill': '#eee',
     'weight': 5,
     'opacity': 1,
-    // 'dashArray': 10,
-    // 'lineCap': 'square',
     'clickable': false
   };
-  var popup = L.popup({closeButton: false})
-    .setContent('<p>Proposed pipeline route</p>');
 
   function onEachFeature(feature, layer) {
-    console.log(feature);
-    // layer.on({
-    //   mouseover: addHighlight,
-    //   mouseout: removeHighlight
-    // });
+    var popup = L.popup();
+    popup.setContent(
+      '<h2>' + feature.properties.Name + '</h2>' +
+      '<ul>' +
+      '<li>' + 'Risk of a medium pipeline spill: ' + feature.properties.spill_risk_medium + '%' + '</li>' +
+      '<li>' +'Risk of a large pipeline spill: ' + feature.properties.spill_risk_large + '%' + '</li>' +
+      '<li>' +'Risk of a medium or large pipeline spill: ' + feature.properties.spill_risk_medium_or_large + '%' + '</li>' +
+      '</ul>'
+    );
+    layer.bindPopup(popup);
   }
-
-  // function addHighlight(e) {
-  //   var layer = e.target;
-
-  //   layer.bindPopup(popup).openPopup();
-  //   layer.setStyle({
-  //     opacity: 1,
-  //   });
-
-  //   if (!L.Browser.ie && !L.Browser.opera) {
-  //     layer.bringToBack();
-  //   }
-  // }
-
-  // function removeHighlight(e) {
-  //   var layer = e.target;
-
-  //   layer.closePopup();
-  //   layer.setStyle({
-  //     opacity: 0.65,
-  //   });
-
-  //   if (!L.Browser.ie && !L.Browser.opera) {
-  //     layer.bringToFront();
-  //   }
-  // }
 
   var routeLayer = L.geoJson(null, {
     style: function(feature) {
@@ -60,10 +33,8 @@ $(function() {
   });
 
   $.getJSON('js/pipeline_sections_data.json', function(data) {
-    // var routeGeojson = topojson.feature(data, data.objects.pipeline);
     routeLayer.addData(data);
   });
 
   map.addLayer(routeLayer);
-
 });
